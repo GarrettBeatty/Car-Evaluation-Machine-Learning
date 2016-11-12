7af9339df7a009c4545d2f19354e27f90453a367
Garrett Beatty <gbt@localhost.localdomain>
2016-11-12 18:15:18 -0500

    initial

---
+++ car_eval.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import numpy as np 
+from sklearn import preprocessing, model_selection, svm
+
+df = pd.read_csv('car.data.txt')
+df = df.apply(preprocessing.LabelEncoder().fit_transform)
+
+X = np.array(df.drop(['class'],1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, train_size = 0.8)
+
+clf = svm.LinearSVC()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
